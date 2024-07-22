5a339c4380f590f7b6a19cfd40051fcca13f6d8c
mohammad fawzy <moha.fawzy63@gmail.com>
2024-07-22 10:06:45 +0300

    add more tests

--- tests/test_basic_calc.py
+++ tests/test_basic_calc.py
@@ -4,21 +4,38 @@ import unittest
 from basic_calculator.basic_calc import BasicCalculator
 from sympy.abc import x
 
+# python -m unittest tests/test_basic_calc.py 
 
 class TestBasicCalculator(unittest.TestCase):
-    """Test cases for the Function class."""
+    """Test cases for the BasicCalculator class."""
 
     def test_expression(self):
         """Test the expression property."""
-        function = BasicCalculator("1+6")
-        self.assertEqual(function.expression, 1+6)
+        basic_calc = BasicCalculator("1+6")
+        self.assertEqual(basic_calc.expression, 1+6)
+
+        basic_calc = BasicCalculator("0+6")
+        self.assertEqual(basic_calc.expression, 0+6)
+
+        basic_calc = BasicCalculator("2+-6")
+        self.assertEqual(basic_calc.expression, 2+-6)
+
+        basic_calc = BasicCalculator("36")
+        self.assertEqual(basic_calc.expression, 36)
 
     def test_evaluation(self):
         """Test the evaluate method."""
-        function = BasicCalculator("1+1")
-        self.assertEqual(function.evaluate_expression(), 2.000)
-        function = BasicCalculator("3^2")
-        self.assertEqual(function.evaluate_expression(), 9.000)
+        basic_calc = BasicCalculator("1+1")
+        self.assertEqual(basic_calc.evaluate_expression(), 2.000)
+        basic_calc = BasicCalculator("3^2")
+        self.assertEqual(basic_calc.evaluate_expression(), 9.000)
+
+    def test_is_valid_expression(self):
+        """Test the evaluate method."""
+        basic_calc = BasicCalculator("1+1")
+        self.assertEqual(basic_calc.is_valid_expression("1+1"), True)
+        basic_calc = BasicCalculator("3^jj2")
+        self.assertEqual(basic_calc.is_valid_expression("3^hh2"), True)
 
 
 if __name__ == '__main__':
