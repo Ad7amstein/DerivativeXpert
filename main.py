ad6fb7b67c5e4723f253ae0e30eee053e59a1f14
Ad7amstein <adham32003200@gmail.com>
2024-07-25 18:25:27 +0300

    Complete the rest of the options

--- main.py
+++ main.py
@@ -4,33 +4,36 @@ import sys
 from basic_calculator.basic_calc import BasicCalculator
 from advanced_calc.function import Function
 
+
 def basic_calculator():
     """Basic calculator function."""
     while True:
         try:
-            exp= input("Enter the exepression or 'm' to main menu:")
-            if (exp== "m"):
+            exp = input("Enter the exepression or 'm' to main menu: ")
+            if exp == "m":
                 return
-            basicCalculator = BasicCalculator(exp)
-        except:
-            print("oooopppps, Invalid expression")
+            basic_calc = BasicCalculator(exp)
+        except ValueError as e:
+            print(f"oooopppps, {str(e)}")
             continue
-        print(basicCalculator.evaluate_expression())
-
+        print(basic_calc.evaluate_expression())
 
 
 def advanced_calculator():
     """Advanced calculator function."""
+    fun = ""
     while True:
-        try:
-            fun= input("Enter the function or 'm' to main menu:")
-            if (fun== "m"):
-                return
-            function = Function(fun)
-        except:
-            print("oooopppps, Invalid Function")
-            continue
-        print("""
+        if fun == "":
+            try:
+                fun = input("Enter the function or 'm' to main menu: ")
+                if fun == "m":
+                    return
+                function = Function(fun)
+            except ValueError as e:
+                print(f"oooopppps, {str(e)}")
+                continue
+        print(
+            """
 1- Evaluate at point
 2- Find Derivative (1,2,..)
 3- Plot
@@ -41,52 +44,79 @@ def advanced_calculator():
 8- Inflection points
 9- Concavity
 10- Asymptotes
-""")
-        choice= input("Enter the choice or 'm' to main menu:")
-        if (choice== "1"):
-            x= input("Enter x: ")
+"""
+        )
+        choice = input("Enter the choice or 'm' to main menu: ")
+        if choice == "1":
+            x = input("Enter x: ")
             try:
                 print(function.evaluate(x))
-            except ValueError as e :
+            except ValueError as e:
                 print(str(e))
-                
-        elif choice =="2":
+        elif choice == "2":
             try:
-                order= int(input("Enter the order of derivative you want: "))
-            except:
+                order = int(input("Enter the order of derivative you want: "))
+            except ValueError:
                 print("Invalid number, please enter correct number.")
             print(function.diffrentiate(order=order).expression)
-        elif choice =="3":
+        elif choice == "3":
             function.plot()
-        elif choice =="4":
+        elif choice == "4":
             try:
-                order= int(input("Enter the order of derivative you want to plot: "))
-            except:
+                order = int(input("Enter the order of derivative you want to plot: "))
+            except ValueError:
                 print("Invalid number, please enter correct number.")
             function.diffrentiate(order=order).plot()
-        elif choice =="5":
+        elif choice == "5":
+            point = input("Enter the point(x): ")
             try:
-                point= int(input("Enter the point(x): "))
-            except:
-                print("Invalid point, please enter correct number(x).")
-            function.slope(point= point)
-        # elif choice =="6":
-        # elif choice =="7":
-        # elif choice =="8":
-        # elif choice =="9":
+                print(function.slope(value=point))
+            except ValueError as e:
+                print(f"Invalid point, {str(e)}")
+        elif choice == "6":
+            critical_points = function.critical_points()
+            if critical_points is None:
+                print("No critical points found.")
+            else:
+                print("Critical points: ", critical_points)
+        elif choice == "7":
+            print(
+                "Interval of increasing and decreasing: ",
+                function.intervals_of_increase_decreasing(),
+            )
+        elif choice == "8":
+            inflection_points = function.inflection_points()
+            if inflection_points is None:
+                print("No inflection points found.")
+            else:
+                print("Inflection points: ", inflection_points)
+        elif choice == "9":
+            print("Concavity: ", function.concavity())
+        elif choice == "10":
+            print("Asymptotes: ", function.asymptotes())
+        elif choice.lower() == "m":
+            return
+        else:
+            print("Invalid choice, please enter correct choice.")
+
 
 def main():
     """Main function for DerivativeXpert."""
     print("\t\t**DerivativeXpert**\n")
 
-    print("""DerivativeXpert:
+    print(
+        """DerivativeXpert:
           is the ultimate tool for basic and advanced calculus,
           including plotting, derivatives, limits, and more.
-          It provides a comprehensive suite of features to assist with various calculus operations,
-          making it an essential tool for students and professionals alike.""")
+          It provides a comprehensive suite of features to
+          assist with various calculus operations,
+          making it an essential tool for students and professionals alike."""
+    )
 
     while True:
-        print("\nDerivativeXpert is a command-line tool that provides the following features:")
+        print(
+            "\nDerivativeXpert is a command-line tool that provides the following features:"
+        )
         print("\n1. Basic Calculator: Perform basic arithmetic operations.")
         print("2. Advanced Calculator: Perform advanced calculus operations.")
         print("3. Exit: Exit the program.")
